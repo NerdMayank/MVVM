e40cafc4983e8c1ec7a4743abdcbd65a9efa9c77
Mayank Pradhan <nerdmyk@gmail.com>
2023-05-02 15:49:27 +0530

    (ViewModel & ViewModelFactory)

---
+++ ViewModelFactory/app/src/main/java/com/example/viewmodelfactory/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewmodelfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var txt:TextView
+    lateinit var mainViewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainViewModel= ViewModelProvider(this,MainViewModelFactory(10))[MainViewModel::class.java]
+        txt=findViewById(R.id.text)
+        setText()
+    }
+
+    private fun setText() {
+        txt.text=mainViewModel.count.toString()
+    }
+
+    fun increment(view: View) {
+        mainViewModel.increment()
+        setText()
+    }
+
+}
